d0a4dbb8ed476050a984499b549986104b6816f3
Hugh Rundle <hugh@hughrundle.net>
2020-02-29 15:02:59 +1100

    update user dashboard
    Updates vuejs components to work better and provide cleaner code.
    Resolves #12
    Resolves #21
    Makes a start on #22

--- public/scripts/user-vue.js
+++ public/scripts/user-vue.js
@@ -1,10 +1,14 @@
 Vue.component('message-list', {
-  // el: '#user-messages',
   props: ['messages'],
   template: `
 <div v-if="messages" id="user-messages">
   <ul v-for="msg in messages" class="blog-list">
-    <li v-bind:class="msg.class">{{ msg.text }}</li>
+    <li v-bind:class="msg.class">
+      <span class="message-text">{{ msg.text }}</span>
+      <span class="flash-close" v-on:click="removeMessage(msg)">
+      X
+      </span>
+    </li>
   </ul>
 </div>
   `,
@@ -15,9 +19,7 @@ Vue.component('message-list', {
   },
   methods: {
     removeMessage(msg) {
-      // TODO:
-      // fired when click on X to get rid of it
-      // reloading the page will also remove any messages that aren't in the DB
+      this.$emit('remove-message', msg)
     }
   },
   mounted () {
@@ -26,19 +28,12 @@ Vue.component('message-list', {
   }
 })
 
-var userInfo =  new Vue({
-  el: '#user-info',
+Vue.component('user-info', {
+  props: ['user', 'legacy', 'messages'],
   data() {
     return {
-      user: {
-        email: null,
-        twitter: null,
-        mastodon: null,
-        pocket: false,
-        admin: false
-      },
       editing: false,
-      messages: []
+      user: this.user
     }
   },
   methods: {
@@ -77,21 +72,58 @@ var userInfo =  new Vue({
       })
     }
   },
-  mounted () {
-    axios
-    .get('/api/v1/user/info')
-    .then(response => {
-      this.user = response.data
-      if (response.data.error) {
-        this.addMessage(response.data.error) // if the user is not registered yet
-      }
-    })
-    .catch( err => this.user = 'error')
-  }
+  template: `
+  <section>
+    <div v-if="user && user.admin">
+      <button class="" type="button" onclick="location.href='/admin'">Admin</button>
+    </div>
+    <h3 class="form-label">Your Info</h3>
+    <form v-if="editing" name="user-info" method="POST">
+      <div>
+        <label for="email">Email:</label>
+        <input id="email" name="email" v-model="user.email">
+      </div>
+      <div>
+        <label for="twitter">Twitter:</label>
+        <input id="twitter" name="twitter" v-model="user.twitter">
+      </div>
+      <div>
+        <label for="mastodon">Mastodon:</label>
+        <input id="mastodon" name="mastodon" v-model="user.mastodon">
+      </div>
+      <button class="update-button" v-on:click.prevent="updateUser" id="update-button">Update</button>
+      <button class="update-button" v-on:click="editing = false">Cancel</button>
+    </form>
+    <div v-else>
+      <div class="user-info">
+        <div id="email">
+          <span class="form-label">Email: </span>
+          <span v-if="user">{{ user.email }}</span>
+        </div>
+        <div id="twitter">
+          <span class="form-label">Twitter: </span>
+          <span v-if="user">{{ user.twitter }}</span>
+        </div>
+        <div id="mastodon">
+          <span class="form-label">Mastodon: </span>
+          <span v-if="user">{{user.mastodon }}</span>
+        </div>
+      </div>
+      <button v-on:click="editing = true">Edit</button>
+    </div>
+    <form id="pocket" class="pocket-info">
+      <template v-if="user && user.pocket">
+        <p>You are subscribed to receive articles straight to your <strong>{{ user.pocket.username }}</strong> Pocket list. Nice one!</p>
+        <button v-on:click="this.cancelPocket" type="button">Cancel Pocket Subscription</button>
+      </template>
+      <button v-else type="button" onclick="location.href='/user/pocket'">Subscribe via Pocket</button>
+    </form>
+  </section>
+  `
 })
 
-var userBlogs =  new Vue({
-  el: '#user-approved-blogs',
+Vue.component('user-approved-blogs', {
+  props: ['messages'],
   data () {
     return {
       userIdString: null,
@@ -163,88 +195,68 @@ var userBlogs =  new Vue({
       blog.editing = true
       Vue.set(this.blogs, this.blogs.indexOf(blog), blog)
     }
-  }
-})
-
-Vue.component('register-or-claim-blogs', {
-  props: ['url', 'category'],
-  data () {
-    return {
-      categories: blogCategories,
-      legacy: legacy
-    }
   },
   template: `
-  <form id="registerBlog">
-    <label for="url">URL:</label>
-    <input v-model="url" type="url" name="url" size="60"><br/>
-    <label for="category">Category:</label>
-    <select v-model="category" name="category">
-    <option v-for="cat in categories" v-bind:value="cat">{{ cat }}</option>
-    </select>
-    <button v-on:click.prevent="validateBlog('register')">Register blog</button>
-  </form>
-  `,
-    methods: {
-      validateBlog(action) {
-        this.$emit('validate-url', 
-        {
-          url: this.url, 
-          category: this.category, 
-          action: action
-        })
-      }
-    }
-  })
-  
-var userUnapprovedBlogs =  new Vue({
-  el: '#user-unapproved-blogs',
+  <section>
+  <ul class="blog-list">
+    <li v-for='blog in blogs' class="listed-blog" v-bind:class="{deleting: blog.deleting, editing: blog.editing}" v-bind:key="blog.id">
+      <div class="approved-blog"></div>
+      <span v-if="blog.title"><a v-bind:href="blog.url">{{ blog.title }}</a></span>
+      <span v-else>{{ blog.url }}</span>
+      <form class="blog-editing-form" v-if="blog.editing" name="blog-info" method="POST">
+        <label for="category">Category:</label>
+        <select v-model="blog.category" name="category">
+          <option v-for="cat in blogCategories" v-bind:value="cat">{{ cat }}</option>
+        </select>
+        <button class="" v-on:click.prevent="editBlog(blog, blogs.indexOf(blog))" id="update-button">Confirm update</button>
+        <button class="" v-on:click.prevent="cancelEditing(blog, blogs.indexOf(blog))">Cancel</button>
+      </form>
+      <button class="" v-else v-bind:class="{hidden: blog.deleting}" v-on:click="checkingEditing(blog)" v-bind:id="blog.idString">Update</button>
+      <button class="" v-if="blog.deleting" v-on:click="deleteBlog(blog)" v-bind:id="blog.idString">Confirm deletion</button>
+      <span v-else-if="blog.editing"></span>
+      <button class="" v-else v-on:click="checkingDeletion(blog)" v-bind:id="blog.idString">Delete</button>
+    </li>
+  </ul>
+  </section>  
+`
+})
+
+Vue.component('register-blog', {
+  props: ['messages', 'ublogs'],
   data () {
     return {
-      uBlogs : [],
-      category: null,
+      messages: this.messages,
+      categories: blogCategories,
+      registering: false,
       url: null,
-      messages: []
+      category: null,
+      ublogs: this.ublogs
     }
-  },
-  mounted () {
-    axios
-    .get('/api/v1/user/unapproved-blogs')
-    .then(response => {
-      this.uBlogs = response.data
-    })
-    .catch( err => messages.push({class: 'flash-error', text: err}))
   },
   methods: {
     addMessage(msg) {
       this.messages.push(msg)
     },
-    validate(args) {
-      if (!this.validateUrl(args.url)) { 
+    validateUrl(input) {
+      var regex = /http(s)?:\/\/([a-z0-9-_~:\/?#[\]@!$&'()*+,;=]*)(\.([a-z0-9-_~:\/?#[\]@!$&'()*+,;=]+)+)+/i
+      return regex.test(input)
+    },
+    validate() {
+      if (!this.validateUrl(this.url)) { 
         msg = {
           class: 'flash-error',
           text: 'please enter a valid url in the form http://example.com'
         }
         this.addMessage(msg)
-      } else if (!args.category) {
+      } else if (!this.category) {
         msg = {
           class: 'flash-error',
           text: 'you must select a category for your blog'
         }
         this.addMessage(msg)
       } else {
-        if (args.action === 'register') {
-          this.registerBlog(args.url, args.category)
-        } else if (args.action === 'claim') {
-          this.claimBlog(args.url, args.category)
-        } else {
-          console.error('no action specified')
-        }
+        this.registerBlog(this.url, this.category)
       }
-    },
-    validateUrl(input) {
-      var regex = /http(s)?:\/\/([a-z0-9-_~:\/?#[\]@!$&'()*+,;=]*)(\.([a-z0-9-_~:\/?#[\]@!$&'()*+,;=]+)+)+/i
-      return regex.test(input)
     },
     registerBlog(url, category) {
       axios
@@ -258,32 +270,103 @@ var userUnapprovedBlogs =  new Vue({
        // if we get an ok status, we just update the list, 
        // rather than sending the full data back and forth
         if (response.data.status === 'ok') {
-          Vue.set(this.uBlogs, this.uBlogs.length, {url: url, approved: false}) // add to the end of the blogs list
+          this.$emit( 'update-ublogs', {url: url, approved: false})
+          this.registering = false
         }
         this.url = null
         this.category = null
       })
       .catch(err => {
+        console.log(err)
         this.addMessage({class: 'flash-error', text: err.message})
       })
+    }
+  },
+  template: `
+  <section>
+    <form v-if="registering" class="register-blog">
+      <label for="url">URL:</label>
+      <input v-model="url" type="url" name="url" size="60"><br/>
+      <label for="category">Category:</label>
+      <select v-model="category" name="category">
+      <option v-for="cat in categories" v-bind:value="cat">{{ cat }}</option>
+      </select>
+      <button class="register-blog-button" v-on:click.prevent="validate()">Register blog</button>
+      <button v-on:click="registering = false">Cancel</button>
+      </form>
+    <button v-else v-on:click="registering = true">Register a new blog</button>
+  </section>
+  `
+  })
+
+Vue.component('user-unapproved-blogs', {
+  props: ['messages', 'ublogs'],
+  data () {
+    return {
+      ublogs : this.ublogs,
+      category: null,
+      url: null
+    }
+  },
+  mounted () {
+  
+  },
+  methods: {
+    addMessage(msg) {
+      this.messages.push(msg)
+    }
+  },
+  template: `
+  <section>
+  <ul class="blog-list" >
+    <li v-for='blog in ublogs' v-bind:key="blog.idString" class="listed-blog">
+      <div class="unapproved-blog"></div>
+      <span v-if="blog.title"><a v-bind:href="blog.url">{{ blog.title }}</a></span>
+      <span v-else>{{ blog.url }}</span>
+      <span class="awaiting-approval"> - awaiting approval</span>
+    </li>
+  </ul>
+  </section>
+  `
+})
+
+new Vue({
+  el: '#main',
+  data () {
+    return {
+      legacy: true,
+      messages: [],
+      user: null,
+      ublogs: []
+    }
+  },
+  mounted () {
+    // TODO: here we should grab into about whether the system is legacy?
+    axios
+    .get('/api/v1/user/info')
+    .then(response => {
+      this.user = response.data
+      if (response.data.error) {
+        this.messages.push(response.data.error) // if the user is not registered yet
+      }
+    })
+    .catch( err => this.user = 'error')
+
+    axios
+    .get('/api/v1/user/unapproved-blogs')
+    .then(response => {
+      this.ublogs = response.data
+      // TODO: $emit a uBlogs update?
+    })
+    .catch( err => this.messages.push({class: 'flash-error', text: err}))
+  },
+  methods: {
+    updateUblogs(args) {
+      Vue.set(this.ublogs, this.ublogs.length, args) // add to the end of the blogs list
     },
-    claimBlog(url, category) {
-      axios
-      .post('/api/v1/update/user/claim-blog', {
-        url: url,
-        category: category
-      })
-      .then( response => {
-        this.addMessage(response.data)
-        if (response.data.class === 'flash-success') {
-          Vue.set(this.uBlogs, this.uBlogs.length, {url: url, approved: false}) // add to the end of the unapproved blogs list
-        }
-        this.url = null
-        this.category = null
-      })
-      .catch(err => {
-        this.addMessage({class: 'flash-error', text: err.message})
-      })
+    removeMessage(msg) {
+      // fired when click on X to get rid of it
+      Vue.delete(this.messages, this.messages.indexOf(msg))
     }
   }
 })
